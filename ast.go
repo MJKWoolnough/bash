32d870fe49fe9f73c7d916b4da4b46f4ba132b2a
Michael Woolnough <michael.woolnough@gmail.com>
2025-02-25 18:16:02 +0000

    added initial parsing entry function

--- ast.go
+++ ast.go
@@ -1,2 +1,23 @@
 // Package bash implements a bash tokeniser and AST.
 package bash // import "vimagination.zapto.org/bash"
+
+// Parse parses Bash input into AST.
+func Parse(t Tokeniser) (*File, error) {
+	p, err := newBashParser(t)
+	if err != nil {
+		return nil, err
+	}
+
+	f := new(File)
+	if err = f.parse(p); err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+type File struct{}
+
+func (f *File) parse(p *bashParser) error {
+	return nil
+}
