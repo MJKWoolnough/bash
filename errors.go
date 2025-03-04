5ba650eb0d4ab96512e7f94a54995fda7ec71c7d
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-04 21:20:20 +0000

    added error

--- errors.go
+++ errors.go
@@ -8,4 +8,5 @@ var (
 	ErrInvalidCharacter      = errors.New("invalid character")
 	ErrInvalidNumber         = errors.New("invalid number")
 	ErrInvalidAssignment     = errors.New("invalid assignment")
+	ErrMissingClosingBracket = errors.New("missing closing bracket")
 )
