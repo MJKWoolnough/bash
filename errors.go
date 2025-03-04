7db74080d8f8333f8ecfb55836b34fd210195fad
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-04 21:45:09 +0000

    remove now unused error

--- errors.go
+++ errors.go
@@ -4,7 +4,6 @@ import "errors"
 
 // Errors.
 var (
-	ErrInvalidBraceExpansion = errors.New("invalid brace expansion")
 	ErrInvalidCharacter      = errors.New("invalid character")
 	ErrInvalidNumber         = errors.New("invalid number")
 	ErrInvalidAssignment     = errors.New("invalid assignment")
