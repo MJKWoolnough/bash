eefb1a55a304f88a7e1793eed5a874af50e269c0
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-18 20:52:04 +0000

    artithmetic expansion is a valid word part

--- ast.go
+++ ast.go
@@ -426,7 +426,7 @@ func nextIsWordPart(b *bashParser) bool {
 		return false
 	case TokenPunctuator:
 		switch tk.Data {
-		case "$(", "${", "`":
+		case "$((", "$(", "${", "`":
 			return true
 		}
 
