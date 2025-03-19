96ef196518b1a3422a774850c262c22259b98db4
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-19 21:18:29 +0000

    added error

--- errors.go
+++ errors.go
@@ -4,10 +4,11 @@ import "errors"
 
 // Errors.
 var (
-	ErrInvalidCharacter      = errors.New("invalid character")
-	ErrInvalidNumber         = errors.New("invalid number")
-	ErrInvalidAssignment     = errors.New("invalid assignment")
-	ErrMissingClosingBracket = errors.New("missing closing bracket")
-	ErrInvalidEndOfStatement = errors.New("invalid end of statement")
-	ErrIncorrectBacktick     = errors.New("incorrect backtick depth")
+	ErrInvalidCharacter          = errors.New("invalid character")
+	ErrInvalidParameterExpansion = errors.New("invalid parameter expansion")
+	ErrInvalidNumber             = errors.New("invalid number")
+	ErrInvalidAssignment         = errors.New("invalid assignment")
+	ErrMissingClosingBracket     = errors.New("missing closing bracket")
+	ErrInvalidEndOfStatement     = errors.New("invalid end of statement")
+	ErrIncorrectBacktick         = errors.New("incorrect backtick depth")
 )
