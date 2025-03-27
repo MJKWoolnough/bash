0b91c2662503aa49a3db949d26d4e8a00192b24b
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-27 20:37:01 +0000

    implemented WordPart.printSource

--- format_print.go
+++ format_print.go
@@ -2,18 +2,42 @@ package bash
 
 import "io"
 
-func (ArithmeticExpansion) printSource(w io.Writer, v bool) {}
-func (Assignment) printSource(w io.Writer, v bool)          {}
-func (Command) printSource(w io.Writer, v bool)             {}
-func (CommandSubstitution) printSource(w io.Writer, v bool) {}
-func (File) printSource(w io.Writer, v bool)                {}
-func (ParameterAssign) printSource(w io.Writer, v bool)     {}
-func (ParameterExpansion) printSource(w io.Writer, v bool)  {}
-func (Parameter) printSource(w io.Writer, v bool)           {}
-func (Pipeline) printSource(w io.Writer, v bool)            {}
-func (Redirection) printSource(w io.Writer, v bool)         {}
-func (Statement) printSource(w io.Writer, v bool)           {}
-func (String) printSource(w io.Writer, v bool)              {}
-func (Value) printSource(w io.Writer, v bool)               {}
-func (WordPart) printSource(w io.Writer, v bool)            {}
-func (Word) printSource(w io.Writer, v bool)                {}
+func (a ArithmeticExpansion) printSource(w io.Writer, v bool) {}
+
+func (a Assignment) printSource(w io.Writer, v bool) {}
+
+func (c Command) printSource(w io.Writer, v bool) {}
+
+func (c CommandSubstitution) printSource(w io.Writer, v bool) {}
+
+func (f File) printSource(w io.Writer, v bool) {}
+
+func (p ParameterAssign) printSource(w io.Writer, v bool) {}
+
+func (p ParameterExpansion) printSource(w io.Writer, v bool) {}
+
+func (p Parameter) printSource(w io.Writer, v bool) {}
+
+func (p Pipeline) printSource(w io.Writer, v bool) {}
+
+func (r Redirection) printSource(w io.Writer, v bool) {}
+
+func (s Statement) printSource(w io.Writer, v bool) {}
+
+func (s String) printSource(w io.Writer, v bool) {}
+
+func (ve Value) printSource(w io.Writer, v bool) {}
+
+func (wp WordPart) printSource(w io.Writer, v bool) {
+	if wp.Part != nil {
+		io.WriteString(w, wp.Part.Data)
+	} else if wp.ArithmeticExpansion != nil {
+		wp.ArithmeticExpansion.printSource(w, v)
+	} else if wp.CommandSubstitution != nil {
+		wp.CommandSubstitution.printSource(w, v)
+	} else if wp.ParameterExpansion != nil {
+		wp.ParameterExpansion.printSource(w, v)
+	}
+}
+
+func (wd Word) printSource(w io.Writer, v bool) {}
