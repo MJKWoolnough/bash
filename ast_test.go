cd91ccb9502db305ef28baa599eb3938d0bd6fa8
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-31 19:40:15 +0100

    added WordOrToken error test

--- ast_test.go
+++ ast_test.go
@@ -401,6 +401,41 @@ func TestWordOrToken(t *testing.T) {
 				Tokens: tk[:1],
 			}
 		}},
+		{"$(||)", func(t *test, tk Tokens) { // 3
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err: Error{
+									Err: Error{
+										Err: Error{
+											Err:     ErrMissingWord,
+											Parsing: "Command",
+											Token:   tk[1],
+										},
+										Parsing: "Pipeline",
+										Token:   tk[1],
+									},
+									Parsing: "Statement",
+									Token:   tk[1],
+								},
+								Parsing: "File",
+								Token:   tk[1],
+							},
+							Parsing: "CommandSubstitution",
+							Token:   tk[1],
+						},
+						Parsing: "WordPart",
+						Token:   tk[0],
+					},
+					Parsing: "Word",
+					Token:   tk[0],
+				},
+				Parsing: "WordOrToken",
+				Token:   tk[0],
+			}
+		}},
 	}, func(t *test) (Type, error) {
 		var w WordOrToken
 
