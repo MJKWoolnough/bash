e1110b0664de8834aeaccafa8d6ebad1df2155a0
Michael Woolnough <michael.woolnough@gmail.com>
2025-04-03 21:25:38 +0100

    added Value error tests

--- ast_test.go
+++ ast_test.go
@@ -215,6 +215,76 @@ func TestValue(t *testing.T) {
 				Tokens: tk[2:9],
 			}
 		}},
+		{"a=$(||)", func(t *test, tk Tokens) { // 6
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err: Error{
+									Err: Error{
+										Err: Error{
+											Err:     ErrMissingWord,
+											Parsing: "Command",
+											Token:   tk[3],
+										},
+										Parsing: "Pipeline",
+										Token:   tk[3],
+									},
+									Parsing: "Statement",
+									Token:   tk[3],
+								},
+								Parsing: "File",
+								Token:   tk[3],
+							},
+							Parsing: "CommandSubstitution",
+							Token:   tk[3],
+						},
+						Parsing: "WordPart",
+						Token:   tk[2],
+					},
+					Parsing: "Word",
+					Token:   tk[2],
+				},
+				Parsing: "Value",
+				Token:   tk[2],
+			}
+		}},
+		{"a=($(||))", func(t *test, tk Tokens) { // 7
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err: Error{
+									Err: Error{
+										Err: Error{
+											Err:     ErrMissingWord,
+											Parsing: "Command",
+											Token:   tk[4],
+										},
+										Parsing: "Pipeline",
+										Token:   tk[4],
+									},
+									Parsing: "Statement",
+									Token:   tk[4],
+								},
+								Parsing: "File",
+								Token:   tk[4],
+							},
+							Parsing: "CommandSubstitution",
+							Token:   tk[4],
+						},
+						Parsing: "WordPart",
+						Token:   tk[3],
+					},
+					Parsing: "Word",
+					Token:   tk[3],
+				},
+				Parsing: "Value",
+				Token:   tk[3],
+			}
+		}},
 	}, func(t *test) (Type, error) {
 		var v Value
 
