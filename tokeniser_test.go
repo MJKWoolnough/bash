5fc97d515532b12789c0aba5808748c42091254f
Michael Woolnough <michael.woolnough@gmail.com>
2025-04-12 19:36:35 +0100

    added tokeniser tests for 'special' identifiers

--- tokeniser_test.go
+++ tokeniser_test.go
@@ -58,7 +58,7 @@ func TestTokeniser(t *testing.T) {
 			},
 		},
 		{ // 7
-			"$ident $name a\\nbc=a $0 $12 a$b a${b}c",
+			"$ident $name a\\nbc=a $0 $12 a$b a${b}c $$ $! $?",
 			[]parser.Token{
 				{Type: TokenIdentifier, Data: "$ident"},
 				{Type: TokenWhitespace, Data: " "},
@@ -79,6 +79,12 @@ func TestTokeniser(t *testing.T) {
 				{Type: TokenIdentifier, Data: "b"},
 				{Type: TokenPunctuator, Data: "}"},
 				{Type: TokenWord, Data: "c"},
+				{Type: TokenWhitespace, Data: " "},
+				{Type: TokenIdentifier, Data: "$$"},
+				{Type: TokenWhitespace, Data: " "},
+				{Type: TokenIdentifier, Data: "$!"},
+				{Type: TokenWhitespace, Data: " "},
+				{Type: TokenIdentifier, Data: "$?"},
 				{Type: parser.TokenDone, Data: ""},
 			},
 		},
