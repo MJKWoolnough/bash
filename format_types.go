3b3e08f9f51a185a356a5f57cb2b6583c915827f
Michael Woolnough <michael.woolnough@gmail.com>
2025-05-31 08:36:04 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -755,6 +755,9 @@ func (f *PatternLines) printType(w io.Writer, v bool) {
 	pp.Print("\nCaseTerminationType: ")
 	f.CaseTerminationType.printType(&pp, v)
 
+	pp.Print("\nComments: ")
+	f.Comments.printType(&pp, v)
+
 	pp.Print("\nTokens: ")
 	f.Tokens.printType(&pp, v)
 
