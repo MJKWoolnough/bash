124186aab6e741848171f9595318e5e9850f4fd3
Michael Woolnough <michael.woolnough@gmail.com>
2025-06-01 10:39:09 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -587,6 +587,9 @@ func (f *LoopCompound) printType(w io.Writer, v bool) {
 	pp.Print("\nFile: ")
 	f.File.printType(&pp, v)
 
+	pp.Print("\nComments: ")
+	f.Comments.printType(&pp, v)
+
 	pp.Print("\nTokens: ")
 	f.Tokens.printType(&pp, v)
 
